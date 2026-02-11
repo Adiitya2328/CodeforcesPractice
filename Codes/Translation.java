cd74e855f6eafdf881a5f165fdb4394aa8d3774c
Adiitya2328 <adityachoudhary7898@gmail.com>
2026-02-11 21:19:26 +0530

    implied the method of two pointer to check whether the two strings are palindrome or not, to solve Translation question

---
+++ Codes/Translation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class  Translation{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+
+        int i = 0;
+        int j = t.length()-1;
+        boolean isSame = true;
+
+
+        while(i< s.length()){
+            if(s.charAt(i) != t.charAt(j)){
+                isSame = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+
+       if(isSame ){
+        System.out.println("YES");
+       } else{
+        System.out.println("NO");
+       }
+    }
+}
