1def2ea4dffdc7995991c70b2482d01029bb6049
Adiitya2328 <adityachoudhary7898@gmail.com>
2026-03-28 23:43:09 +0530

    solved

---
+++ Codes/calculatingFunction.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class calculatingFunction{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n =sc.nextLong();
+        long ans;
+        if (n%2 == 0){
+            ans = n/2;
+        }else{
+            ans = -(n+1)/2;
+        }
+        System.out.println(ans);
+    }
+}
