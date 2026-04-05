425b28ef3b5b6435fa076ea88f8262fa312edb7a
Adiitya2328 <adityachoudhary7898@gmail.com>
2026-04-05 22:37:07 +0530

    solved div4 problem

---
+++ Codes/Integer67Problem.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Integer67Problem{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0 ; i<t ; i++){
+            int n = sc.nextInt();
+
+            System.out.println(n);
+        }
+    }
+}
