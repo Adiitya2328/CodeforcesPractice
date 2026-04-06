48661dedf1ae9ec44dc3f28276a80c17d6dd8c0a
Adiitya2328 <adityachoudhary7898@gmail.com>
2026-04-06 23:42:40 +0530

    solved

---
+++ Codes/CandiesAndSister.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class CandiesAndSister{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0 ;i<t ; i++){
+            int n= sc.nextInt();
+            System.out.println((n-1)/2);
+        }
+        
+
+    }
+}
